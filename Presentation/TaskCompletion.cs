7cbb4ea8043456b27472ac471910e5c8116d086e
danielmarbach <daniel.marbach@openplace.net>
2017-10-18 09:59:34 +0200

    Better spacing

--- Presentation/TaskCompletion.cs
+++ Presentation/TaskCompletion.cs
@@ -8,11 +8,14 @@ class TaskCompletion : IRunnable
     public async Task Run()
     {
         var taskCompletionSource = new TaskCompletionSource<bool>();
+        
         var simulator = new Simulator();
         simulator.Fired += (sender, args) => taskCompletionSource.TrySetResult(true);
         this.PrintStart();
         simulator.Start();
+
         await taskCompletionSource.Task.ConfigureAwait(false);
+
         this.PrintEnd();
     }
 }
