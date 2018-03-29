27d2e61c6e10dd697856fb09b5ba79a40505794b
danielmarbach <daniel.marbach@openplace.net>
2018-03-29 10:42:25 +0200

    StackTrace improvements

---
+++ StackTrace/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            await Level1();
+        } catch (Exception ex) 
+        {
+            Console.WriteLine(ex.StackTrace);
+        }
+    }
+
+    static async Task Level1() {
+        await Level2();
+    }
+
+    static async Task Level2() {
+        await Level3();
+    }
+
+    static async Task Level3() {
+        await Level4();
+    }
+
+    static async Task Level4() {
+        await Level5();
+    }    
+
+    static async Task Level5() {
+        await Level6();
+    }       
+
+    static async Task Level6() {
+        await Task.Yield();
+        throw new Exception("boom");
+    }
+}
